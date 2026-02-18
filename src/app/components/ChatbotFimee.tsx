cd5b1416041d7346273d8bed146529d3242ac6bf
ErickQuispe123 <erickquispepillaca@gmail.com>
2026-02-18 12:03:59 -0500

    Update ChatbotFimee.tsx

--- src/app/components/ChatbotFimee.tsx
+++ src/app/components/ChatbotFimee.tsx
@@ -98,7 +98,7 @@ function GenerarPrompt(entrada: string):string {
         `;
 }
 
-const APIKEY = "AIzaSyBmW-7fqLQwJegpyKDmke0rBlg6lOe_pLk";
+const APIKEY = "AIzaSyAn-nGxY-szijOUzHhM2gniCtddEYhbqQg";
 let session = new GoogleGenAI({
   apiKey: APIKEY
 })
